be8f51494ad8812ce0ecb93926dec307e80f7f3b
raopremrao <raom61337@gmail.com>
2026-04-01 21:06:47 +0530

    feat: added folder structure and requirements

---
+++ config/parameters.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Simulation parameters
+TIME_STEPS = 200
+TOTAL_TIME = 5
+
+# Noise parameters
+gamma_amplitude = 0.2
+gamma_phase = 0.15
+gamma_depolarizing = 0.1
+
+# Initial State
+psi0 = np.array([1, 0])
