b4005746648c1e8dfc716217ea7c79d1f6fe0b75
raopremrao <raom61337@gmail.com>
2026-04-02 23:21:19 +0530

    fix: Remove bugs

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from control.control_analysis import compare_control_pulse
 from control.optimal_control import optimize_pulse
 from control.plot_optimal_pulse import plot_optimal_pulse
 
+
+
 # Time
 tlist = np.linspace(0, 5, 200)
 
