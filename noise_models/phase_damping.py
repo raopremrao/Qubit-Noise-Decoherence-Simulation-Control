53a1572771c2e7c66684f519f5061a824f606aa8
raopremrao <raom61337@gmail.com>
2026-04-03 22:56:56 +0530

    Docstring for Phase damping

--- noise_models/phase_damping.py
+++ noise_models/phase_damping.py
@@ -2,5 +2,25 @@ import numpy as np
 from qutip import *
 
 def phase_damping(gamma):
+    '''
+    Phase damping (dephasing) noise model.
+
+    Physics:
+    Phase damping causes loss of coherence without energy loss.
+    Related to T2 dephasing time.
+
+    Collapse operator:
+        L = sqrt(gamma) * sigma_z
+
+    Parameters
+    ----------
+    gamma : float
+        Damping rate (related to T2 time).
+    
+    Returns
+    -------
+    qutip.Qobj
+        Collapse operator for phase damping.
+    '''
     sz = sigmaz()
     return np.sqrt(gamma) * sz
