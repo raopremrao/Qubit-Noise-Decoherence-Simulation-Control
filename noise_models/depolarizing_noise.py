eaec5ac44aecce288287e5c19b347fe583d4719c
raopremrao <raom61337@gmail.com>
2026-04-03 23:06:24 +0530

    Docstring for Depolarizing noise

--- noise_models/depolarizing_noise.py
+++ noise_models/depolarizing_noise.py
@@ -2,6 +2,28 @@ from qutip import *
 import numpy as np
 
 def depolarizing_noise(gamma):
+    '''
+    Depolarizing noise model.
+
+    Physics:
+    Depolarizing noise randomly applies Pauli errors X, Y, Z,
+    pushing the qubit towards a maximally mixed state.
+
+    Collapse Operator:
+        sqrt(gamma) * sigma_x
+        sqrt(gamma) * sigma_y
+        sqrt(gamma) * sigma_z
+
+    Parameters
+    ----------
+    gamma : float
+        Depolarizing rate.
+    
+    Returns
+    -------
+    qutip.Qobj
+        List of collapse operators for depolarizing noise
+    '''
     return [
         np.sqrt(gamma) * sigmax(),
         np.sqrt(gamma) * sigmay(),
