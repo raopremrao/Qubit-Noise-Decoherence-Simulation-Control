b9a357f0082002180f43949d2651ea0a262540fd
raopremrao <raom61337@gmail.com>
2026-04-03 22:52:38 +0530

    Docstring for Amplitude damping

--- noise_models/amplitude_damping.py
+++ noise_models/amplitude_damping.py
@@ -2,5 +2,26 @@ from qutip import *
 import numpy as np
 
 def amplitude_damping(gamma):
+    '''
+    Amplitude damping noise model.
+
+    Physics:
+    Amplitude damping represents energy relaxation  (T1 decay),
+    where the qubit decays from |1> -> |0>.
+
+    Collapse operator:
+        L = sqrt(gamma) * sigma_minus
+
+    Parameters
+    ----------
+    gamma : float
+        Damping rate (related to T1 time).
+
+    Returns
+    -------
+    qutip.Qobj
+        Collapse operator for amplitude damping.
+    '''
+
     sm = destroy(2)
     return np.sqrt(gamma) * sm
