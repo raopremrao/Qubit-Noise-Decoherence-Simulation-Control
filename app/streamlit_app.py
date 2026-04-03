ad965ff7dbc623d019728cbf67421888d5ab1438
raopremrao <raom61337@gmail.com>
2026-04-03 15:03:23 +0530

    feat: Improvements

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -11,6 +11,8 @@ from noise_models.amplitude_damping import amplitude_damping
 from noise_models.phase_damping import phase_damping
 from noise_models.depolarizing_noise import depolarizing_noise
 from simulation.lindblad_solver import lindblad_evolution
+from control.control_pulse import constant_pulse, sinusodial_pulse, gaussian_pulse
+from control.controlled_evolution import run_controlled_simulation
 
 st.set_page_config(page_title="Quantum Noise Simulator", layout="wide")
 
@@ -28,6 +30,9 @@ noise_strength = st.sidebar.slider("Noise Strength", 0.0, 1.0, 0.2)
 
 time = st.sidebar.slider("Simulation Time", 1, 10, 5)
 
+use_control = st.sidebar.checkbox("Enable Control Pulse")
+pulse_type = st.sidebar.selectbox("Control Pulse Type", ["Constant", "Sinusodial", "Gaussian"])
+
 # Simulation
 tlist = np.linspace(0, time, 200)
 H = 0.5 * sigmax()
@@ -40,9 +45,22 @@ elif noise_type == "Phase Damping":
 else:
     c_ops = depolarizing_noise(noise_strength)
 
-states = lindblad_evolution(H, psi0, c_ops, tlist)
+# With Control
+if use_control:
+    if pulse_type == "Constant":
+        pulse_func = constant_pulse
+    elif pulse_type == "Sinusodial":
+        pulse_func = sinusodial_pulse
+    else:
+        pulse_func = gaussian_pulse
+    
+    tlist, states = run_controlled_simulation(pulse_func, c_ops)
+else:
+    states = lindblad_evolution(H, psi0, c_ops, tlist)
 
-target = basis(2, 1)
+# Fidelity
+# target = basis(2, 1)
+target = ket2dm(basis(2, 1))
 fids = [fidelity(state, target) for state in states]
 
 # Layout
@@ -77,3 +95,42 @@ st.write(states[-1])
 # st.write("Matrix Form:")
 # st.dataframe(rho)
 
+# Fidelity vs Noise Strength
+st.subheader("Fidelity vs Noise Strength")
+
+noise_vals = np.linspace(0, 1, 20)
+fid_noise = []
+
+# H_noise = 0 * sigmaz() # No Driving Hamiltonian
+
+for g in noise_vals:
+    if noise_type == "Amplitude Damping":
+        c_ops_temp = [amplitude_damping(g)]
+    elif noise_type == "Phase Damping":
+        c_ops_temp = [phase_damping(g)]
+    else:
+        c_ops_temp = depolarizing_noise(g)
+
+    if use_control:
+        _, states_temp = run_controlled_simulation(pulse_func, c_ops_temp)
+    else:
+        states_temp = lindblad_evolution(H, psi0, c_ops_temp, tlist)
+        
+    fid_noise.append(fidelity(states_temp[-1], target))
+
+fig3, ax = plt.subplots()
+ax.plot(noise_vals, fid_noise)
+ax.axvline(x=noise_strength, color='r', linestyle='--', label=f'Current: {noise_strength}')
+ax.set_xlabel("Noise Strength")
+ax.set_ylabel("Fidelity")
+ax.grid(True)
+ax.set_title("Fidelity vs Noise Strength")
+ax.legend()
+st.pyplot(fig3)
+
+# Download Plot
+# st.download_button(
+#     label="Download Fidelity Plot",
+#     data=open("results/plots/fidelity_plot.png", "rb").read(),
+#     file_name="fidelity_plot.png"
+# )
